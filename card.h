d52be8133ae9d1f3f27b8532a8e32774faf2cb8a
squawk7x <andreas@ottburg.de>
2025-02-26 18:22:13 +0100

    Ui Layouts used, findchild(...) removed

--- card.h
+++ card.h
@@ -14,56 +14,57 @@ extern QVector<QString> suitnames;
 
 enum class CardType { Small, Normal };
 
-class Card : public QPushButton
-{
-    Q_OBJECT
+class Card : public QPushButton {
+  Q_OBJECT
 
-private:
-    QString suit_;
-    QString rank_;
-    std::pair<QString, QString> pair_;
-    QString suitname_;
-    QString rankname_;
-    QString str_;
-    int value_;
-    CardType cardType_;
+ private:
+  QString suit_;
+  QString rank_;
+  std::pair<QString, QString> pair_;
+  QString suitname_;
+  QString rankname_;
+  QString str_;
+  int value_;
+  CardType cardType_;
 
-public:
-    explicit Card(const QString& suit, const QString& rank, QWidget* parent = nullptr);
-    explicit Card(const QString& cardStr, QWidget* parent = nullptr);
-    explicit Card(const std::pair<QString, QString>& pair, QWidget* parent = nullptr);
-    Card(const Card& other);                // Copy constructor
-    Card& operator=(const Card& other);     // Copy assignment operator
-    Card(Card&& other) noexcept;            // Move constructor
-    Card& operator=(Card&& other) noexcept; // Move assignment operator
-    virtual ~Card() = default;              // Default Deconstructor
+ public:
+  explicit Card(const QString& suit, const QString& rank,
+                QWidget* parent = nullptr);
+  explicit Card(const QString& cardStr, QWidget* parent = nullptr);
+  explicit Card(const std::pair<QString, QString>& pair,
+                QWidget* parent = nullptr);
+  Card(const Card& other);                 // Copy constructor
+  Card& operator=(const Card& other);      // Copy assignment operator
+  Card(Card&& other) noexcept;             // Move constructor
+  Card& operator=(Card&& other) noexcept;  // Move assignment operator
+  virtual ~Card() = default;               // Default Deconstructor
 
-    bool operator==(const Card& other) const;
-    bool operator<(const Card& other) const;
-    bool operator>(const Card& other) const;
-    QSharedPointer<Card> clone(QWidget* parent = nullptr) const;
+  bool operator==(const Card& other) const;
+  bool operator<(const Card& other) const;
+  bool operator>(const Card& other) const;
+  QSharedPointer<Card> clone(QWidget* parent = nullptr) const;
 
-    // Getters
-    QString suit() const;
-    QString rank() const;
-    QString suitname() const;
-    QString rankname() const;
-    QString str() const;
-    int value() const;
+  // Getters
+  QString suit() const;
+  QString rank() const;
+  QString suitname() const;
+  QString rankname() const;
+  QString str() const;
+  int value() const;
 
-    //Setters
-    void setCardType(CardType type);
-    void loadImage(bool isCardFaceVisible = true);
+  // Setters
+  void setCardType(CardType type);
+  void loadImage(bool isCardFaceVisible = true);
 
-private:
-    void initCard();
-    void setSuitname(const QString& suit);
-    void setRankname(const QString& rank);
-    void setStr();
-    void setValue(const QString& rank);
+ private:
+  void initCard();
+  void setSuitname(const QString& suit);
+  void setRankname(const QString& rank);
+  void setStr();
+  void setValue(const QString& rank);
 
-signals:
-    void cardClicked(const Card& card);
+ signals:
+  void cardClicked(const Card& card);
 };
 
 #endif // CARD_H
--- table.cpp
+++ table.cpp
@@ -51,114 +51,46 @@ void Table::initializeGame(int numberOfPlayers)
     game_ = QSharedPointer<Game>::create(numberOfPlayers);
 
     // Find all QHBoxLayouts defined in table.ui
-    QHBoxLayout* layoutPlayer2 = findChild<QHBoxLayout*>("layoutPlayer2");
+    // QHBoxLayout* layoutPlayer2 = findChild<QHBoxLayout*>("layoutPlayer2");
     QHBoxLayout* layoutPlayer3 = nullptr;
     if (numberOfPlayers == 3) {
-        layoutPlayer3 = findChild<QHBoxLayout*>("layoutPlayer3");
+      layoutPlayer3 = findChild<QHBoxLayout*>("layoutPlayer3");
     }
 
-    QHBoxLayout* layoutMonitor = findChild<QHBoxLayout*>("layoutMonitor");
-    QHBoxLayout* layoutEightsChooser = findChild<QHBoxLayout*>("layoutEightsChooser");
-    QHBoxLayout* layoutQuteChooser = findChild<QHBoxLayout*>("layoutQuteChooser");
-    QHBoxLayout* layoutJpointsChooser = findChild<QHBoxLayout*>("layoutJpointsChooser");
-    QHBoxLayout* layoutRoundChooser = findChild<QHBoxLayout*>("layoutRoundChooser");
-    QHBoxLayout* layoutGot1 = findChild<QHBoxLayout*>("layoutGot1");
-    QHBoxLayout* layoutGot2 = findChild<QHBoxLayout*>("layoutGot2");
-    QHBoxLayout* layoutPlayed = findChild<QHBoxLayout*>("layoutPlayed");
-    QHBoxLayout* layoutDrawn = findChild<QHBoxLayout*>("layoutDrawn");
-    QHBoxLayout* layoutShuffles = findChild<QHBoxLayout*>("layoutShuffles");
-    QHBoxLayout* layoutBlind = findChild<QHBoxLayout*>("layoutBlind");
-    QHBoxLayout* layoutJsuitChooser = findChild<QHBoxLayout*>("layoutJsuitChooser");
-    QHBoxLayout* layoutStack = findChild<QHBoxLayout*>("layoutStack");
-    QHBoxLayout* layoutPlayable = findChild<QHBoxLayout*>("layoutPlayable");
-    QHBoxLayout* layoutRound = findChild<QHBoxLayout*>("layoutRound");
-    QHBoxLayout* layoutScores = findChild<QHBoxLayout*>("layoutScores");
-    QHBoxLayout* layoutPlayer1 = findChild<QHBoxLayout*>("layoutPlayer1");
-    QPushButton* pbNext = findChild<QPushButton*>("pbNext");
-    QPushButton* pbDraw = findChild<QPushButton*>("pbDraw");
-    QPushButton* pbInfo = findChild<QPushButton*>("pbInfo");
-
     // Add widgets to their respective layouts
-    layoutPlayer2->addWidget(game_->player2()->handdeck().get());
+    ui->gbPlayer2Layout->addWidget(game_->player2()->handdeck().get());
     if (numberOfPlayers == 3) {
         layoutPlayer3->addWidget(game_->player3()->handdeck().get());
     }
-
-    layoutMonitor->addWidget(game_->monitor_.get());
-    layoutEightsChooser->addWidget(game_->eightsChooser().get());
-    layoutQuteChooser->addWidget(game_->quteChooser().get());
-    layoutJpointsChooser->addWidget(game_->jpointsChooser().get());
-    layoutRoundChooser->addWidget(game_->roundChooser().get());
-    layoutGot1->addWidget(game_->got1().get());
-    layoutGot2->addWidget(game_->got2().get());
-    layoutPlayed->addWidget(game_->played().get());
-    layoutDrawn->addWidget(game_->drawn().get());
-    layoutShuffles->addWidget(game_->lcdShuffles().get());
-    layoutBlind->addWidget(game_->blind().get());
-    layoutJsuitChooser->addWidget(game_->jsuitChooser().get());
-
-    layoutStack->addWidget(game_->stack().get());
-
-    layoutPlayable->addWidget(game_->playable().get());
-    layoutRound->addWidget(game_->lcdRound().get());
-    layoutScores->addWidget(game_->lcdP1().get());
-    layoutScores->addWidget(game_->lcdP2().get());
+    ui->gbMonitorLayout->addWidget(game_->monitor_.get());
+    ui->gbEightsChooserLayout->addWidget(game_->eightsChooser().get());
+    ui->gbQuteChooserLayout->addWidget(game_->quteChooser().get());
+    ui->gbJpointsChooserLayout->addWidget(game_->jpointsChooser().get());
+    ui->gbRoundChooserLayout->addWidget(game_->roundChooser().get());
+    ui->gbGot1Layout->addWidget(game_->got1().get());
+    ui->gbGot2Layout->addWidget(game_->got2().get());
+    ui->gbPlayedLayout->addWidget(game_->played().get());
+    ui->gbDrawnLayout->addWidget(game_->drawn().get());
+    ui->gbShufflesLayout->addWidget(game_->lcdShuffles().get());
+    ui->gbBlindLayout->addWidget(game_->blind().get());
+    ui->gbJsuitChooserLayout->addWidget(game_->jsuitChooser().get());
+    ui->gbStackLayout->addWidget(game_->stack().get());
+    ui->gbPlayableLayout->addWidget(game_->playable().get());
+    ui->gbRoundLayout->addWidget(game_->lcdRound().get());
+    ui->gbScoresLayout->addWidget(game_->lcdP1().get());
+    ui->gbScoresLayout->addWidget(game_->lcdP2().get());
     if (numberOfPlayers == 3) {
-        layoutScores->addWidget(game_->lcdP3().get());
+      ui->gbScoresLayout->addWidget(game_->lcdP3().get());
     }
-    layoutPlayer1->addWidget(game_->player1()->handdeck().get());
+    ui->gbPlayer1Layout->addWidget(game_->player1()->handdeck().get());
 
-    // Add group boxes to their respective layouts
-    QGroupBox* groupBoxPlayer2 = findChild<QGroupBox*>("gbPlayer2");
-    groupBoxPlayer2->setLayout(layoutPlayer2);
-
-    QGroupBox* groupBoxPlayer3 = findChild<QGroupBox*>("gbPlayer3");
     if (numberOfPlayers == 3) {
-        groupBoxPlayer3->setLayout(layoutPlayer3);
-        groupBoxPlayer3->show();
+      ui->gbPlayer3->show();
+      ui->gbGot2->show();
     } else {
-        groupBoxPlayer3->hide();
+      ui->gbPlayer3->hide();
+      ui->gbGot2->hide();
     }
-
-    QGroupBox* groupBoxMonitor = findChild<QGroupBox*>("gbMonitor");
-    groupBoxMonitor->setLayout(layoutMonitor);
-    QGroupBox* groupBoxEightsChooser = findChild<QGroupBox*>("gbEightsChooser");
-    groupBoxEightsChooser->setLayout(layoutEightsChooser);
-    QGroupBox* groupBoxQuteChooser = findChild<QGroupBox*>("gbQuteChooser");
-    groupBoxQuteChooser->setLayout(layoutQuteChooser);
-    QGroupBox* groupBoxJpointsChooser = findChild<QGroupBox*>("gbJpointsChooser");
-    groupBoxJpointsChooser->setLayout(layoutJpointsChooser);
-    QGroupBox* groupBoxRoundChooser = findChild<QGroupBox*>("gbRoundChooser");
-    groupBoxRoundChooser->setLayout(layoutRoundChooser);
-    QGroupBox* groupBoxGot1 = findChild<QGroupBox*>("gbGot1");
-    groupBoxGot1->setLayout(layoutGot1);
-    QGroupBox* groupBoxGot2 = findChild<QGroupBox*>("gbGot2");
-    if (numberOfPlayers == 3) {
-        groupBoxGot2->setLayout(layoutGot2);
-        groupBoxGot2->show();
-    } else {
-        groupBoxGot2->hide();
-    }
-    QGroupBox* groupBoxPlayed = findChild<QGroupBox*>("gbPlayed");
-    groupBoxPlayed->setLayout(layoutPlayed);
-    QGroupBox* groupBoxDrawn = findChild<QGroupBox*>("gbDrawn");
-    groupBoxDrawn->setLayout(layoutDrawn);
-    QGroupBox* groupBoxShuffles = findChild<QGroupBox*>("gbShuffles");
-    groupBoxShuffles->setLayout(layoutShuffles);
-    QGroupBox* groupBoxBlind = findChild<QGroupBox*>("gbBlind");
-    groupBoxBlind->setLayout(layoutBlind);
-    QGroupBox* groupBoxJsuitChooser = findChild<QGroupBox*>("gbJsuitChooser");
-    groupBoxJsuitChooser->setLayout(layoutJsuitChooser);
-    QGroupBox* groupBoxStack = findChild<QGroupBox*>("gbStack");
-    groupBoxStack->setLayout(layoutStack);
-    QGroupBox* groupBoxPlayable = findChild<QGroupBox*>("gbPlayable");
-    groupBoxPlayable->setLayout(layoutPlayable);
-    QGroupBox* groupBoxRound = findChild<QGroupBox*>("gbRound");
-    groupBoxRound->setLayout(layoutRound);
-    QGroupBox* groupBoxScores = findChild<QGroupBox*>("gbScores");
-    groupBoxScores->setLayout(layoutScores);
-    QGroupBox* groupBoxPlayer1 = findChild<QGroupBox*>("gbPlayer1");
-    groupBoxPlayer1->setLayout(layoutPlayer1);
 
     connect(game_.get(), &Game::setRbNumPlayers, this, [&](int num) {
         if (num == 2) {
@@ -244,9 +176,12 @@ void Table::initializeGame(int numberOfPlayers)
     QObject::connect(game_.get(), &Game::paintDrawButton, this, &Table::onPaintDrawButton);
 
     // Pushbuttons
-    QObject::connect(pbNext, &QPushButton::clicked, this, &Table::onNextClicked);
-    QObject::connect(pbDraw, &QPushButton::clicked, this, &Table::onDrawClicked);
-    QObject::connect(pbInfo, &QPushButton::clicked, this, &Table::onInfoClicked);
+    QObject::connect(ui->pbNext, &QPushButton::clicked, this,
+                     &Table::onNextClicked);
+    QObject::connect(ui->pbDraw, &QPushButton::clicked, this,
+                     &Table::onDrawClicked);
+    QObject::connect(ui->pbInfo, &QPushButton::clicked, this,
+                     &Table::onInfoClicked);
 
     ui->rbCardTypeSmall->click();
 }
